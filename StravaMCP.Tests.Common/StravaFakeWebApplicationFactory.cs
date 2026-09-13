33b1d8bc5e2dae029804960cd2630877a9b3ff07
Jkotoun <Jkotoun58@gmail.com>
2026-09-14 00:00:35 +0200

    fix: make integration tests independent of local Strava user-secrets

--- StravaMCP.Tests.Common/StravaFakeWebApplicationFactory.cs
+++ StravaMCP.Tests.Common/StravaFakeWebApplicationFactory.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StravaMCP.Strava;
 
@@ -13,6 +14,19 @@ public sealed class StravaFakeWebApplicationFactory<TProgram> : WebApplicationFa
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            var options = StravaOptionsFixture.Create();
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Strava:ClientId"] = options.ClientId,
+                ["Strava:ClientSecret"] = options.ClientSecret,
+                ["Strava:RefreshToken"] = options.RefreshToken,
+                ["Strava:ApiBaseUrl"] = options.ApiBaseUrl,
+                ["Strava:TokenUrl"] = options.TokenUrl,
+            });
+        });
+
         builder.ConfigureServices(services =>
         {
             services.AddHttpClient("StravaAuth")
